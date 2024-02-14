fb043ecb4353cf71ae06dae8cc7685d190678bc7
Oleg Belyakov <OBelyakov@inno.tech>
2024-02-14 21:50:20 +0300

    initial

---
+++ src/main/java/ru/stepup/AccessLogParser/MainApp.java
@@ -0,0 +1,8 @@
+package ru.stepup.AccessLogParser;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Случайное число от 0 до 1: "
+                + Math.random());
+    }
+}
